f012ec3dedbf89974533ef949e58e4367fbd0fd0
Miklos Daniel Matyas <matyasmiklos@gmail.com>
2023-03-27 08:20:11 +0200

    feat: add Day Picker to My Day

--- src/MyDays/my-days.tsx
+++ src/MyDays/my-days.tsx
@@ -1,5 +1,7 @@
 import { useCreateMyDay } from "./my-days.api";
 import { MyDayContainer } from "./components";
+import { DayPicker } from "../common/day-picker/DayPicker";
+import { useState } from "react";
 
 export const MyDays = () => {
   const myDay = {
@@ -11,7 +13,9 @@ export const MyDays = () => {
   const createMyDayHandler = async () => {
     await createMyDay(myDay);
   };
+
+  const [day, setDay] = useState(new Date((new Date()).setHours(0, 0, 0, 0)));
   return <MyDayContainer>
-    createMyday
+    <DayPicker date={day} onDateChange={setDay} />
   </MyDayContainer>;
 };
